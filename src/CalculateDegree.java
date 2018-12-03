e99bb1e7f5ecb88a0c00ea232314daa77a344d48
Cristto <cristto.tolosa@gmail.com>
2018-12-03 18:23:39 -0500

    Some changes

--- src/CalculateDegree.java
+++ src/CalculateDegree.java
@@ -4,6 +4,7 @@ import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.List;
 
 public class CalculateDegree {
 
@@ -14,6 +15,12 @@ public class CalculateDegree {
     private HashSet<String> nodeValues;
 
     private HashSet<Integer> degrees;
+
+    private String origin;
+
+    private ArrayList<String> unCheckedValues;
+
+    private int degree = 0;
 
     CalculateDegree() {
 //        try {
@@ -49,9 +56,9 @@ public class CalculateDegree {
     }
 
     private void automaticInitialize() {
-        peopleNumber = 4;
+        peopleNumber = 5;
 //        System.out.println("Persons: " + peopleNumber);
-        relationsNumber = 4;
+        relationsNumber = 5;
 //        System.out.println("Relations number: " + relationsNumber);
 
         getAutomaticRelations();
@@ -89,20 +96,55 @@ public class CalculateDegree {
         relations = new ArrayList<>(relationsNumber);
         nodes = new HashMap<>(peopleNumber);
         degrees = new HashSet<>();
-        nodeValues.add("Ashok");
-        nodeValues.add("Kiyoshi");
-        nodeValues.add("Chun");
-        nodeValues.add("Ursala");
 
-        PeopleRelation peopleRelation1 = new PeopleRelation("Ashok", "Kiyoshi");
-        PeopleRelation peopleRelation2 = new PeopleRelation("Ursala", "Chun");
-        PeopleRelation peopleRelation3 = new PeopleRelation("Ursala", "Kiyoshi");
-        PeopleRelation peopleRelation4 = new PeopleRelation("Kiyoshi", "Chun");
+        nodeValues.add("Andres");
+        nodeValues.add("Diego");
+        nodeValues.add("Felipe");
+        nodeValues.add("Mario");
+        nodeValues.add("Pacho");
+        nodeValues.add("Cristhian");
+
+        PeopleRelation peopleRelation1 = new PeopleRelation("Andres", "Diego");
+        PeopleRelation peopleRelation2 = new PeopleRelation("Diego", "Mario");
+        PeopleRelation peopleRelation3 = new PeopleRelation("Diego", "Felipe");
+        PeopleRelation peopleRelation4 = new PeopleRelation("Mario", "Felipe");
+        PeopleRelation peopleRelation5 = new PeopleRelation("Mario", "Pacho");
+        PeopleRelation peopleRelation6 = new PeopleRelation("Pacho", "Cristhian");
 
         relations.add(peopleRelation1);
         relations.add(peopleRelation2);
         relations.add(peopleRelation3);
         relations.add(peopleRelation4);
+        relations.add(peopleRelation5);
+        relations.add(peopleRelation6);
+
+
+//        nodeValues.add("Ashok");
+//        nodeValues.add("Chun");
+//        nodeValues.add("Ursala");
+//        nodeValues.add("Kiyoshi");
+//
+//        PeopleRelation peopleRelation1 = new PeopleRelation("Ashok", "Chun");
+//        PeopleRelation peopleRelation2 = new PeopleRelation("Ursala", "Kiyoshi");
+//
+//        relations.add(peopleRelation1);
+//        relations.add(peopleRelation2);
+
+
+//        nodeValues.add("Ashok");
+//        nodeValues.add("Chun");
+//        nodeValues.add("Ursala");
+//        nodeValues.add("Kiyoshi");
+//
+//        PeopleRelation peopleRelation1 = new PeopleRelation("Ashok", "Kiyoshi");
+//        PeopleRelation peopleRelation2 = new PeopleRelation("Ursala", "Chun");
+//        PeopleRelation peopleRelation3 = new PeopleRelation("Ursala", "Kiyoshi");
+//        PeopleRelation peopleRelation4 = new PeopleRelation("Kiyoshi", "Chun");
+//
+//        relations.add(peopleRelation1);
+//        relations.add(peopleRelation2);
+//        relations.add(peopleRelation3);
+//        relations.add(peopleRelation4);
 
         validateRelations();
     }
@@ -139,39 +181,55 @@ public class CalculateDegree {
         int nodesConnected = 0;
         ArrayList<String> nodes = new ArrayList<>(nodeValues);
         for (int i = 0; i < nodes.size(); i++) {
-            boolean nodeConnected = false;
+            boolean nodeConnected;
+            int connections = 0;
+
             for (int j = 0; j < nodes.size(); j++) {
+                unCheckedValues = new ArrayList<>(nodeValues);
+                origin = nodes.get(i);
+                String destiny = nodes.get(j);
                 nodeConnected = isNodeConnectedToDestiny(nodes.get(i), nodes.get(j));
+                if (nodeConnected) {
+                    connections++;
+                } else {
+                    System.out.println("GRAPH DISCONNECTED: Origin: " + origin + " - Destiny: " + destiny);
+                }
             }
 
-            if (nodeConnected) {
+            if (connections == nodeValues.size()) {
                 nodesConnected++;
             }
         }
+
+        System.out.println("nodesConnected: " + nodesConnected + ", elements: " + nodeValues.size());
         boolean graphConnected = (nodesConnected == nodeValues.size());
         System.out.println("IS GRAPH CONNECTED: " + graphConnected);
         return graphConnected;
     }
 
     private boolean isNodeConnectedToDestiny(String referenceNode, String destiny) {
-        boolean isConnected = false;
+        boolean isConnected;
+        degree = 0;
         Node reference = nodes.get(referenceNode);
         if (reference.getChildren().contains(destiny)) {
-            degrees.add(1);
+            degree++;
+            degrees.add(degree);
             isConnected = true;
+            System.out.println("Connection: " + isConnected + ", " + origin + " - " + destiny);
         } else {
-            for (String child : reference.getChildren()) {
-                Node newReference = nodes.get(child);
-                if (newReference.getChildren().contains(destiny)) {
-                    degrees.add(2);
-                    isConnected = true;
-                    break;
-                }
-            }
+            degree = 1;
+            updateUncheckedValues(reference.getValue());
+            isConnected = iterateNetwork(new ArrayList<>(reference.getChildren()), destiny);
         }
 
 //        System.out.println(referenceNode + "IS NODE CONNECTED: " + isConnected);
         return isConnected;
+    }
+
+    private void updateUncheckedValues(String value) {
+        if (!unCheckedValues.isEmpty()) {
+            unCheckedValues.remove(value);
+        }
     }
 
     private int getMaximumDegree() {
@@ -182,5 +240,38 @@ public class CalculateDegree {
             }
         }
         return maxValue;
+    }
+
+    /**
+     *
+     */
+    private boolean iterateNetwork(List<String> children, String destiny) {
+        boolean found = false;
+        degree++;
+        HashSet<String> minorChildren = new HashSet<>();
+        for (String child : children) {
+            Node newReference = nodes.get(child);
+            if (newReference.getChildren().contains(destiny)) {
+                degrees.add(degree);
+                found = true;
+                break;
+            } else {
+                updateUncheckedValues(child);
+                for (String n : newReference.getChildren()) {
+                    if (unCheckedValues.contains(n)) {
+                        minorChildren.add(n);
+                    }
+                }
+//                minorChildren.addAll(newReference.getChildren());
+            }
+        }
+
+        if (!found && !minorChildren.isEmpty()) {
+//            System.out.println("RECURSIVE CALLED");
+            return iterateNetwork(new ArrayList<>(minorChildren), destiny);
+        }
+
+        System.out.println("Connection: " + found + ", " + origin + " - " + destiny);
+        return found;
     }
 }
